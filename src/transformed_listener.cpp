92c630b886cf31bd5812c495c6dc65ebb31fec4c
Albert Enyedy <aenyedy56@gmail.com>
2019-10-07 14:31:42 -0400

    displays many segmented lines simultaneously

--- src/transformed_listener.cpp
+++ src/transformed_listener.cpp
@@ -31,6 +31,11 @@ void point_transformed_CallBack(const sensor_msgs::PointCloud2& transformed_poin
 
   //set up blank pointcloud for copying over the segmented data
   pcl::PointCloud<pcl::PointXYZ> segpc;
+  //blank pointcloud for storing all lines simultaneously
+  pcl::PointCloud<pcl::PointXYZ> totalpc;
+  //blank ROS msgs for segmented pointcloud and total pointcloud
+  sensor_msgs::PointCloud2 segpc2; 
+  sensor_msgs::PointCloud2 totalpc2; 
 
   int line_count =0;
   bool foundLine = true;
@@ -46,7 +51,7 @@ void point_transformed_CallBack(const sensor_msgs::PointCloud2& transformed_poin
     // Mandatory
     seg.setModelType (pcl::SACMODEL_LINE); // looking for lines in the data
     seg.setMethodType (pcl::SAC_RANSAC);
-    seg.setDistanceThreshold (0.01); // perhaps tune this to have higher room for error?
+    seg.setDistanceThreshold (0.05); // perhaps tune this to have higher room for error?
     
     //set the pointcloud that will be source for segmenter
     seg.setInputCloud (recpc);
@@ -54,23 +59,31 @@ void point_transformed_CallBack(const sensor_msgs::PointCloud2& transformed_poin
     //segment the pointcloud, placing results in inliers and coefficients
     seg.segment (*inliers, *coefficients);
     
-    
+    std::cerr << "Inliers " << line_count << std::endl;
+    for (int i=0; i<inliers->indices.size(); i++ ) {
+      std::cerr << inliers->indices[i] << " ";     
+    } 
+    std::cerr << std::endl;     
     
     //make a copy of the pointcloud recpc, using the inliers indices to determine 
     //which points carry over to new cloud and get visualized
     pcl::copyPointCloud(*recpc, *inliers, segpc);  
 
+    pcl::toROSMsg(segpc, segpc2);
 
-    //for (int i=0; i<inliers->indices.size(); i++ ) {
-    //  segpc.at(inliers->indices[i]).y = line_count;
-    //} 
+    pcl::concatenatePointCloud(totalpc2, segpc2, totalpc2);
+
+    // for (int i=0; i<inliers->indices.size(); i++ ) {
+    //   segpc.at(inliers->indices[i]).y = line_count;
+    // } 
 
     pcl::PointIndices::Ptr outliers (new pcl::PointIndices);
 
     for (int j=0; j < 811; j++) {
       if(std::find(inliers->indices.begin(), inliers->indices.end(), j) == inliers->indices.end()) {
          outliers->indices.push_back(j);
-      }     
+      }
+      //std::cerr << "Loop Iteration: " << j << std::endl;     
     }
     //set pointer to new point cloud 
     pcl::copyPointCloud(*recpc, *outliers, *recpc);  
@@ -86,7 +99,8 @@ void point_transformed_CallBack(const sensor_msgs::PointCloud2& transformed_poin
   pcl::toROSMsg(segpc, lined_pointcloud);
 
   //publish the segmented pointcloud
-  mod_transformed_pub.publish(lined_pointcloud);
+  //formerly:mod_transformed_pub.publish(lined_pointcloud); 
+  mod_transformed_pub.publish(totalpc2);
 
 
 }
@@ -99,7 +113,7 @@ int main(int argc, char **argv){
   
   //set up the node's subscriber and specify what topic it is subscribing to (here, "cloud"), the queue of waiting data it can handle (here, 1 for best results), and the operation that will be run when the message is received (here, pointCloudCallBack, the operation we declared at the top of the file) 
   ros::NodeHandle nh;
-  ros::Subscriber transformed_sub = nh.subscribe("modified_pointcloud", 1, point_transformed_CallBack);
+  ros::Subscriber transformed_sub = nh.subscribe("front_view", 1, point_transformed_CallBack);
   
   //setup the node's publisher and specify the topic it will publish, with a queue of 1 for best results
   mod_transformed_pub = nh.advertise<sensor_msgs::PointCloud2>("modified_transformed", 1);
